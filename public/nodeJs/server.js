13f37426c3cbe96c06263b98a81773d11a0a4536
eqwdy <kuksenok.a@internet.ru>
2025-12-02 20:59:23 +0300

    mb will work

--- public/nodeJs/server.js
+++ public/nodeJs/server.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
 
 const PORT = process.env.BOT_PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
